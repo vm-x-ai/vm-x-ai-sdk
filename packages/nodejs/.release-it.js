82eba9c5f5cbb4b991aead4c6ea64b980301ca63
Lucas Vieira <lucas.vieira94@outlook.com>
2024-04-22 06:21:40 -0300

    chore(nodejs): migrate semantic-release to release-it

---
+++ packages/nodejs/.release-it.js
@@ -0,0 +1,39 @@
+const version = '${version}';
+const packageName = 'nodejs';
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      path: '.',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+    '@release-it/bumper': {
+      in: '../../dist/packages/nodejs/package.json',
+      out: '../../dist/packages/nodejs/package.json',
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${packageName}-v${version}`,
+    pushRepo: 'git@github.com:vm-x-ai/vm-x-ai-sdk.git',
+    commitsPath: '.',
+    commitMessage: `feat(${packageName}): released version v${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+  },
+  npm: {
+    publish: false,
+    versionArgs: [],
+  },
+  github: {
+    release: true,
+    releaseName: `${packageName}-v${version}`,
+  },
+  hooks: {
+    'before:git:release': ['git add --all'],
+  },
+};
--- packages/nodejs/release.config.cjs
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('../../release.base.config');
-
-const name = 'nodejs';
-const srcRoot = `packages/nodejs`;
-
-module.exports = defineConfig(name, srcRoot);
