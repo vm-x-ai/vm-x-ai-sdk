1e3940b314912f69081893295d127467283ba901
Lucas Vieira <lucas.vieira94@outlook.com>
2024-04-22 06:41:15 -0300

    chore(nodejs): add nx format before git add

--- packages/nodejs/.release-it.js
+++ packages/nodejs/.release-it.js
@@ -35,7 +35,7 @@ module.exports = {
     releaseName: `${packageName}-v${version}`,
   },
   hooks: {
-    'before:git:release': ['git add --all'],
+    'before:git:release': ['pnpm nx format', 'git add --all'],
     'after:bump': ['pnpm nx run nodejs:build'],
   },
 };
