1bbf94bee8486910a979e6a51dfaabd5b703814f
Lucas Vieira <lucas.vieira94@outlook.com>
2024-10-23 20:08:10 -0300

    fix: read package json from the correct path

--- packages/completion/provider/js/src/version.ts
+++ packages/completion/provider/js/src/version.ts
@@ -2,5 +2,5 @@ import fs from 'fs';
 import path from 'path';
 
 export function getPackageVersion(): string {
-  return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')).version;
+  return JSON.parse(fs.readFileSync(path.resolve(__dirname, './package.json'), 'utf-8')).version;
 }
